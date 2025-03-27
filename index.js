e1dbcddb037c714193efdd4cb9151b2c9571a383
zhangfq <zhangfq@codoon.com>
2025-03-27 15:34:30 +0800

    update4

--- index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// 如果直接运行这个文件，启动服务器
-if (require.main === module) {
-    require('./src/server');
-} else {
-    // 如果作为模块导入，导出服务器
-    module.exports = require('./src/server');
-} 
